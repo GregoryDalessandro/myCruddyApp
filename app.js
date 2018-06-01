34bf0a4c82513882ab5cddcb7cb1cb37efb9bac3
GregoryDalessandro <gregorydalessandro942@gmail.com>
2018-06-01 13:38:49 -0400

    add helper utilities to read and write to local storage

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
+  
   $('.submitForm').on('click', function() {
-    let $textFieldValue = $('.textField').val();
+    let textFieldValue = $('.textField').val();
     $('.debug').text(textFieldValue);
+
+  localStorage.setItem('myFormTextData', textFieldValue);  
+  $('.textField').val('');
   });
+  
+  $('.getData').on('click', function() {
+    let retrieveData = localStorage.getItem('myFormTextData');
+    $('.debug').text(retrieveData);
+  });
+
+  // this is old can be removed
+  // $('.textField').on('keyup', function() {
+  //   let textFieldValue = $('.textField').val();
+  //   $('.debug').text(textFieldValue);
+  // });
 
 });
