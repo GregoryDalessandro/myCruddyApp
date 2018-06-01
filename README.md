46bc2b7677af3069e715352ac06f3278ba3751a2
GregoryDalessandro <gregorydalessandro942@gmail.com>
2018-06-01 14:46:52 -0400

    make textarea handle code, have two fields for name and snippet, store name and code into localStorage

--- README.md
+++ README.md
@@ -5,5 +5,16 @@ CrUD Create Update and Delete App
 Goals:
 
 - [x] Input form with single text input
-- [x] click handler to capture data in text form field
-- [x] helper utilities to read and write to local storage
+- [x] Click handler to capture data in text form field
+- [x] Helper utilities to read and write to local storage
+- [x] Make it handle code! (use a textarea instead of a text)
+- [x] Have two fields, one is a short name the other is the snippet
+- [x] Store the name and code inside localStorage
+- [ ] Display all snippets
+- [ ] Search for a snippet using the name
+- [ ] Add ability to delete data 
+- [ ] Store name and code into an object within local storage, name as prop and code as val
+
+
+<pre>JSON.stringify(myObject.json, null, 2)</pre>
+<code></code>
--- app.js
+++ app.js
@@ -2,15 +2,20 @@ $(document).ready(function() {
   
   $('.submitForm').on('click', function() {
     let textFieldValue = $('.textField').val();
-    $('.debug').text(textFieldValue);
+    let textAreaValue = $('.textArea').val();
+    $('.debug').text('textFieldValue: '+  textFieldValue + ' textAreaValue: ' +  textAreaValue);
 
   localStorage.setItem('myFormTextData', textFieldValue);  
   $('.textField').val('');
+
+  localStorage.setItem('myFormTextAreaData', textAreaValue);  
+  $('.textArea').val('');
   });
   
   $('.getData').on('click', function() {
-    let retrieveData = localStorage.getItem('myFormTextData');
-    $('.debug').text(retrieveData);
+    let retrieveSnippetName = localStorage.getItem('myFormTextData');
+    let retrieveSnippetData = localStorage.getItem('myFormTextAreaData');
+    $('.debug').text('retrieveSnippetName: ' + retrieveSnippetName + ' retrieveSnippetData: ' + retrieveSnippetData );
   });
 
   // this is old can be removed
