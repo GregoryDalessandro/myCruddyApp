d3234e0472daaf39336df910bfa7824b8d53d1ed
GregoryDalessandro <gregorydalessandro942@gmail.com>
2018-06-05 15:16:52 -0400

    fix prevent saving duplicate objects, alert that you already saved this object

--- app.js
+++ app.js
@@ -4,26 +4,25 @@ $(document).ready(function() {
   $('.submitForm').on('click', function() {
     let textFieldValue = $('.textField').val();
     let textAreaValue = $('.textArea').val();
-    
+    let currentSnippet = JSON.stringify({[textFieldValue]: textAreaValue});
+    //console.log(currentSnippet);
+
     if ($('.textField').val() === '' && $('.textArea').val() === '') {
       alert('Please enter a valid name, and fill out form!');
     } else if($('.textField').val() === '') {
       alert('Please enter a valid name!');
     } else if ($('.textArea').val() === '') {
       alert('Please fill out form!');
-    } else if (snippets.data.length === 0) {
-      snippets.data.push({[textFieldValue]: textAreaValue});
-      localStorage.setItem('snippets', JSON.stringify(snippets));    
     } else {
-      snippets.data.forEach(function(snippet) {
-        if (JSON.stringify(snippet) === JSON.stringify({[textFieldValue]: textAreaValue})) {
-          alert('You have already saved!');
-        } else {
-          snippets.data.push({[textFieldValue]: textAreaValue});
-          localStorage.setItem('snippets', JSON.stringify(snippets));
-        }
-      });
+      if (!snippets.data.includes(currentSnippet)) {
+        snippets.data.push(currentSnippet);
+        localStorage.setItem('snippets', JSON.stringify(snippets));
+      } else {
+        alert('snippet already saved!'); 
+      }
     }
+    //console.log(snippets);
+
   $('.textField').val('');
   $('.textArea').val('');
 
@@ -32,40 +31,23 @@ $(document).ready(function() {
     
   });
   
+
   $('.getData').on('click', function() {
     let retrievedSnippets = JSON.parse(localStorage.getItem('snippets'));
-    //console.log(retrievedSnippets);
+   
+    retrievedSnippets.data = retrievedSnippets.data.map(function(snippet) {
+      return JSON.parse(snippet);
+    });
+    
     retrievedSnippets.data.forEach(function(snippet) {
       for (var key in snippet) {
-       if ($('#' + key).length === 0) {
+        if ($('#' + key).length === 0) {
           $('.snippetContainer').append('<div class="snippet" id="' + key +'">'+ snippet[key] + '</div>');     
         }
-        //console.log($('#' + key).length === 0);
       }
-    });
-        console.log($('.snippet'));
+    }); 
   });
-
 });
-
-// stringify and parse json object
-//snippetOne+++”this is the actual snippet”
-// snippetOne+++”this is the actual snippet”—-snippetTwo+++”this is the other snippet”
-// From Me to Everyone: (05:39 PM)
-// Never seen triple operator before
-// From john michelin to Everyone: (05:39 PM)
-// - - -
-// valueFromLocalStorage.split(‘- - -‘)
-// storage.removeItem(keyName
-
-
-
-// {
-// data: []
-// }
-// From john michelin to Everyone: (05:45 PM)
-// [{ snippetName: ‘’, snippetText: ‘’}, …. {}]
-
 
 // tags: []
 // [‘collection’, ‘es6’]
